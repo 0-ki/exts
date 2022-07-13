703056a88af8ebc4fbb53811a629fa61a331ea47
YounggikimA <rladudrl286@naver.com>
2022-07-13 18:26:41 +0900

    Initial commit

---
+++ src/main/java/com/homework/flow/exts/ExtsApplication.java
@@ -0,0 +1,13 @@
+package com.homework.flow.exts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtsApplication.class, args);
+	}
+
+}
